a7f265279398f9cf0811ca869fc328b872f83c01
Kevin Eduardo <rgkevin.lopez@gmail.com>
2015-06-04 12:17:29 -0600

    landing page

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  // latest news plugin
+  var $news_container = $('#news');
+  var $news_slider = $news_container.bxSlider({
+    auto: true,
+    pager: true,
+    nextText: '',
+    prevText: ''
+  });
+
+})(window.jQuery);
